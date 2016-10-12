ac84ec9202ed97eefb7373fab3edf55983291ce7
JMCWM <joao.cunha@wiremaze.com>
2016-10-12 01:16:35 +0100

    master public subtree not working

--- alt.js
+++
@@ -1,4 +0,0 @@
-import Alt from 'alt';
-const alt = new Alt();
-
-module.exports = alt;
--- constants/constants.js
+++
@@ -1,5 +0,0 @@
-const AppConstants = {
-  FIREBASE_HOST: 'https://sweltering-heat-187.firebaseio.com'
-};
-
-export default AppConstants;
