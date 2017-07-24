37cc2d342c1f773c770bb56dd3af0a83c902cfd4
João Cunha <joao.cunha@wiremaze.com>
2017-07-24 16:42:17 +0000

    Removed react-scrollspy and built export

--- _next/49a7cae6-fc92-44cc-9ec5-adfb8c4ecccd/page/about/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/about', function() {
-            var comp = module.exports=webpackJsonp([6],{337:function(e,t,u){e.exports=u(338)},338:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var o=u(4),l=n(o),r=u(113),f=n(r);t.default=function(){return l.default.createElement(f.default,null,"About")}}},[337]);
-            return { page: comp.default }
-          })
-        
