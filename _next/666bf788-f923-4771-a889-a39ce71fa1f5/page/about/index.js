30137d6366ab1619d05ab06acd68837bb16a51c7
João Cunha <joao.cunha@wiremaze.com>
2017-07-25 17:24:09 +0000

    New build with working mobile menu

---
+++ _next/666bf788-f923-4771-a889-a39ce71fa1f5/page/about/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/about', function() {
+            var comp = module.exports=webpackJsonp([6],{344:function(e,t,u){e.exports=u(345)},345:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var o=u(1),l=n(o),r=u(114),f=n(r);t.default=function(){return l.default.createElement(f.default,null,"About")}}},[344]);
+            return { page: comp.default }
+          })
+        
